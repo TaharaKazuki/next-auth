bda1fdd6e712bf43f99f742e5c96d325288f7d0e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-01-21 20:26:35 +0900

    update

--- auth.ts
+++ auth.ts
@@ -1,9 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
-import Github from "next-auth/providers/github"
+
+import { db } from "@/lib/db"
+
+import authConfig from "./auth.config"
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [Github],
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
