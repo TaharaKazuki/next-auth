0ba0d39e0573aee886d9b4e31e24e5a6aa0c5940
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-01-21 15:31:58 +0900

    updaete

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server"
 
+import bcrypt from "bcryptjs"
 import type * as z from "zod"
 
+import { getUserByEmail } from "@/data/user"
+import { db } from "@/lib/db"
 import { RegisterSchema } from "@/schemas"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,5 +14,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" }
   }
 
-  return { success: "Email sent" }
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "Email already in use!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  return { success: "User created!" }
 }
